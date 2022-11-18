cbdc632ca7c7c7cfec89d8c9826c35ed0c9b7bf7
Naris <nrazafimahatratra@softwaymedical.fr>
2022-11-18 12:59:45 +0300

    Fix font on chips

--- components/chip.tsx
+++ components/chip.tsx
@@ -3,7 +3,7 @@ import { Props } from "./props";
 export default function Chip({ children, className }: Props) {
   return (
     <div
-      className={`w-fit py-2 px-4 text-sm text-onPrimary font-bold rounded-2xl ${className}`}
+      className={`w-fit py-2 px-4 text-sm text-onPrimary font-semibold rounded-2xl ${className}`}
     >
       {children}
     </div>
