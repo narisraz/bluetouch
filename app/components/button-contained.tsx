614bf1e78e46648c275ff15eb55a49c993269f36
Naris <nrazafimahatratra@softwaymedical.fr>
2022-12-02 11:02:50 +0300

    Start and End node within buttons

--- app/components/button-contained.tsx
+++ app/components/button-contained.tsx
@@ -1,16 +1,23 @@
+import { RiLoader2Line } from "react-icons/ri";
+
 import type { Props } from "~/components/props";
 
 interface ButtonContainedProps extends Props {
-  type?: "button" | "submit" | "reset" | undefined
+  type?: "button" | "submit" | "reset" | undefined,
+  loading?: boolean,
+  start?: React.ReactNode,
+  end?: React.ReactNode,
 }
 
-export function ButtonContained({ children, className, type }: ButtonContainedProps) {
+export function ButtonContained({ children, className, type, loading, start, end }: ButtonContainedProps) {
   return (
     <button
       type={type}
-      className={`flex cursor-pointer items-center rounded bg-primary py-2 px-4 text-onPrimary duration-200 hover:bg-primary/70 ${className}`}
+      className={`flex cursor-pointer items-center rounded bg-primary py-2 px-4 text-onPrimary duration-200 hover:bg-primary/70 ${className} gap-1`}
     >
+      {loading && <RiLoader2Line className="animate-spin" /> || start}
       {children}
+      {end}
     </button>
   );
 }
--- app/components/button-outlined.tsx
+++ app/components/button-outlined.tsx
@@ -1,11 +1,22 @@
+import { RiLoader2Line } from "react-icons/ri";
+
 import type { Props } from "~/components/props";
 
-export function ButtonOutlined({ children, className }: Props) {
+interface ButtonContainedProps extends Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  loading?: boolean,
+  start?: React.ReactNode,
+  end?: React.ReactNode,
+}
+
+export function ButtonOutlined({ children, className, loading, start, end }: ButtonContainedProps) {
   return (
     <button
-      className={`ml-2 flex cursor-pointer items-center rounded border border-primary/50 bg-surface py-2 px-3 text-primary duration-200 hover:bg-primary/5 ${className}`}
+      className={`ml-2 flex cursor-pointer items-center rounded border border-primary/50 bg-surface py-2 px-3 text-primary duration-200 hover:bg-primary/5 ${className} gap-1`}
     >
+      {loading && <RiLoader2Line className="animate-spin" /> || start}
       {children}
+      {end}
     </button>
   );
 }
