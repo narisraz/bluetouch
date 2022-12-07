0ff1166e100525678c654e477c3cc594bb782739
Naris <nrazafimahatratra@softwaymedical.fr>
2022-12-07 12:06:48 +0300

    Add margin to loading icon

--- app/modules/shared/components/button-outlined.tsx
+++ app/modules/shared/components/button-outlined.tsx
@@ -14,7 +14,7 @@ export function ButtonOutlined({ children, className, loading, start, end }: But
     <button
       className={`ml-2 flex cursor-pointer items-center rounded border border-primary/50 bg-surface py-2 px-3 text-primary duration-200 hover:bg-primary/5 ${className} gap-1`}
     >
-      {loading && <RiLoader2Line className="animate-spin" /> || start}
+      {(loading && <RiLoader2Line className="mr-2 animate-spin" />) || start}
       {children}
       {end}
     </button>
