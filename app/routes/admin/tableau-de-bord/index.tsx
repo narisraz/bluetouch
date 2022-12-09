32959776c91b014f786eac734e2aafffa1bfc731
Naris <nrazafimahatratra@softwaymedical.fr>
2022-12-09 14:21:35 +0300

    Add content to admin dashboard

--- app/routes/admin/tableau-de-bord/index.tsx
+++ app/routes/admin/tableau-de-bord/index.tsx
@@ -1,13 +1,47 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { countOrganismes } from "~/admin/infrastructure/dependencies";
 import { AdminRouteId, getRouteById } from "~/shared/utils/admin.routes";
+
+interface LoaderData {
+  organismesCount: number
+}
+
+export async function loader() {
+  const organismesCount = await countOrganismes.execute()
+
+  if (!organismesCount.isSuccess) {
+    return json<LoaderData>({
+      organismesCount: 0
+    })
+  }
+
+  return json<LoaderData>({
+    organismesCount: organismesCount.data ?? 0
+  })
+}
 
 export function meta() {
   return { title: getRouteById(AdminRouteId.DASHBOARD).name };
 }
 
 export default function TableauDeBord() {
+  const {organismesCount} = useLoaderData<LoaderData>()
   return (
-    <div>
-      Tableau de bord
+    <div className="grid grid-cols-3 gap-4 font-bold">
+      <div className="flex items-center gap-2 rounded bg-primary p-4">
+        <div className="rounded bg-white/20 py-2 px-3 text-4xl font-bold">{organismesCount}</div>
+        <div className="uppercase">Organismes</div>
+      </div>
+      <div className="flex items-center gap-2 rounded bg-success p-4">
+        <div className="rounded bg-white/20 py-2 px-3 text-4xl font-bold">0</div>
+        <div className="uppercase">SAEP</div>
+      </div>
+      <div className="flex items-center gap-2 rounded bg-error p-4">
+        <div className="rounded bg-white/20 py-2 px-3 text-4xl font-bold">0</div>
+        <div className="uppercase">Abonnées</div>
+      </div>
     </div>
   );
 }
